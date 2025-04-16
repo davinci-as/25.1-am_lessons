479ff2b754dc45754675960dfb34b578d85e1f44
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-04-16 19:43:37 -0300

    feat: update text on click

--- app/src/main/java/ar/edu/davinci/a251_am_lessons/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a251_am_lessons/MainActivity.java
@@ -3,6 +3,9 @@ package ar.edu.davinci.a251_am_lessons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateMessage(View view) {
+        //Log.i("test-message", "Actualizando campo de texto");
+        //R.id.welcome_message
+        TextView welcome_message = findViewById(R.id.welcome_message);
+        welcome_message.setText("New Message");
     }
 }
